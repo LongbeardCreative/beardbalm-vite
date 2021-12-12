cdcf583a09f8e06f4cd96ee6ba23599248b7d5e1
Kevin Ang <ka@kevinang.com>
2021-12-12 01:17:33 -0600

    cleanup

--- src/scripts/lib/smoothScroll.ts
+++ src/scripts/lib/smoothScroll.ts
@@ -1,39 +1,56 @@
-export default function smoothScroll() {
-  function handleClick(e: Event) {
-    const currentTarget = e.currentTarget as HTMLAnchorElement;
-    if (
-      location.pathname.replace(/^\//, '') ===
-        currentTarget.pathname.replace(/^\//, '') &&
-      location.hostname == currentTarget.hostname
-    ) {
-      // Figure out element to scroll to
-      const { hash } = currentTarget;
-      let target =
-        (document.querySelector(hash) as HTMLElement) ||
-        (document.querySelector(`[name=${hash.slice(1)}]`) as HTMLElement);
-      // Does a scroll target exist?
-      if (target) {
-        // Only prevent default if animation is actually gonna happen
-        e.preventDefault();
-        const scrollTop =
-          target.getAttribute('role') === 'tabpanel'
-            ? (target.parentNode as HTMLElement)?.offsetTop
-            : target.offsetTop;
-        window.scrollTo({
-          top: scrollTop,
-          behavior: 'smooth',
-        });
-        target.focus();
-      }
-      if (window.location.hash === hash) {
-        window.dispatchEvent(new Event('hashchange'));
-      } else {
-        window.location.hash = hash;
-      }
-    }
+function handleClick(e: Event) {
+  const currentTarget = e.currentTarget as HTMLAnchorElement;
+  const isSameUrl =
+    location.pathname.replace(/^\//, '') ===
+      currentTarget.pathname.replace(/^\//, '') &&
+    location.hostname == currentTarget.hostname;
+
+  if (!isSameUrl) {
+    return;
   }
 
-  const links = document.querySelectorAll('a[href*="#"][data-smooth-scroll]');
+  // Figure out element to scroll to
+  const { hash } = currentTarget;
+
+  // Target can have the specified ID or name attribute
+  let target =
+    (document.querySelector(hash) as HTMLElement) ||
+    (document.querySelector(`[name=${hash.slice(1)}]`) as HTMLElement);
+
+  // Check if target exists
+  if (!target) {
+    return;
+  }
+
+  // Only prevent default if animation is happening
+  e.preventDefault();
+
+  // Get 'top' position of the target
+  const targetTop =
+    target.style.display === 'none'
+      ? (target.parentNode as HTMLElement)?.offsetTop || target.offsetTop
+      : target.offsetTop;
+
+  // Smooth scroll window
+  window.scrollTo({
+    top: targetTop,
+    behavior: 'smooth',
+  });
+
+  // Focus on target
+  target.focus();
+
+  if (window.location.hash === hash) {
+    window.dispatchEvent(new Event('hashchange'));
+  } else {
+    window.location.hash = hash;
+  }
+}
+
+export default function smoothScroll(
+  selector: string = '[data-smooth-scroll]'
+) {
+  const links = document.querySelectorAll(`a[href*="#"]${selector}`);
   links.forEach((link) => {
     link.addEventListener('click', handleClick);
   });
