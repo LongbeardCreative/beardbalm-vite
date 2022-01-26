dfb427fe5393ebcabcdfd7580138955193468bf1
Kevin Ang <ka@kevinang.com>
2022-01-26 12:31:45 -0600

    fix: scope styles

--- src/scripts/lib/development/index.ts
+++ src/scripts/lib/development/index.ts
@@ -12,7 +12,7 @@ interface AlertProps {
 function showConsoleOverlay({ title, text, actions }: AlertProps) {
   document.body.insertAdjacentHTML(
     'beforeend',
-    `<div id="vite-alert" role="alert">
+    `<div id="vite-alert" role="alert" data-vite-styles>
       <div class="vite-alert-dialog">
         <div class="vite-alert-dialog__overlay" onclick="document.getElementById('vite-alert').style.display = 'none'; document.getElementById('vite-badge').style.display = 'flex'"></div>
         <div class="vite-alert-dialog__card">
@@ -27,7 +27,7 @@ function showConsoleOverlay({ title, text, actions }: AlertProps) {
         </div>
       </div>
     </div>
-    <button id="vite-badge" onclick="document.getElementById('vite-alert').style.display = 'flex'; document.getElementById('vite-badge').style.display = 'none'">
+    <button id="vite-badge" onclick="document.getElementById('vite-alert').style.display = 'flex'; document.getElementById('vite-badge').style.display = 'none'" data-vite-styles>
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/></svg>
     </button>
     `
@@ -37,7 +37,7 @@ function showConsoleOverlay({ title, text, actions }: AlertProps) {
 function showFooterOverlay({ text, actions }: AlertProps) {
   document.body.insertAdjacentHTML(
     'beforeend',
-    `<div id="vite-alert" role="alert">
+    `<div id="vite-alert" role="alert" data-vite-styles>
       <div class="vite-alert-bar">
         <div class="vite-alert-bar__inner">
           <div class="vite-alert-bar__text">
