801126f7ca65e6e8f14edee62483968f645fdc9e
Kevin Ang <ka@kevinang.com>
2022-01-27 15:58:51 -0600

    fix: better accessibility

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
