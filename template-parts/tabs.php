7ae7426f0a35df052c60caa1f9ea1d0a0a1aae16
Kevin Ang <ka@kevinang.com>
2022-02-08 06:51:08 -0600

    feat: expand tabs  api

--- template-parts/tabs.php
+++ template-parts/tabs.php
@@ -3,6 +3,8 @@
 $data    = $args && isset($args['data']) ? $args['data'] : '';
 $class   = $args && isset($args['class']) ? $args['class'] : '';
 
+$no_overflow   = $args && isset($args['no_overflow']) ? $args['no_overflow'] : false;
+
 $partial = $args && isset($args['partial']) ? $args['partial'] : false;
 
 $tabs      = '';
@@ -15,12 +17,14 @@ foreach ($data as $t) {
   $is_tabpanel_active = $is_selected ? 'tabpanel--active' : '';
 
   $title    = $t['title'];
+  $icon     = isset($t['icon']) ? $t['icon'] : null;
   $content  = $t['content'];
   $key      = sanitize_key($title);
   $tabindex = $is_selected ? '0' : '-1';
 
   $tabs .= "<button id='tab-$key' class='tab $is_tab_active' role='tab' aria-selected='$is_selected' aria-controls='$key' tabindex='$tabindex' >";
-  $tabs .= $title;
+  $tabs .= $icon ?: null;
+  $tabs .= '<span>' . $title . '</span>';
   $tabs .= '</button>';
 
   $tabpanels .= "<div id='$key' class='tabpanel $is_tabpanel_active' role='tabpanel' aria-labelledby='tab-$key' aria-expanded='$is_selected' >";
@@ -38,10 +42,10 @@ foreach ($data as $t) {
   echo $tabpanels;
 } else { ?>
   <div class="lb-tabs <?php echo $class; ?>">
-    <div class="tabs" role="tablist">
+    <div class="tabs <?php echo $class ? $class . '__tabs' : ''; ?>" role="tablist" <?php echo $no_overflow ? 'data-no-overflow' : ''; ?>>
       <?php echo $tabs; ?>
     </div>
-    <div class="tabpanels">
+    <div class="tabpanels <?php echo $class ? $class . '__tabpanels' : ''; ?>">
       <?php echo $tabpanels; ?>
     </div>
   </div>
