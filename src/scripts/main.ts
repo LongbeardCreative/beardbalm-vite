df316052959f48ea33c548574bc90d356eecdca4
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-05-03 10:14:46 -0500

    updates

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,19 +1,13 @@
-// https://vitejs.dev/config/#build-polyfillmodulepreload
-// using workaround https://github.com/vitejs/vite/issues/4786
-// if (import.meta.env.MODE !== "development") {
-//   import("vite/modulepreload-polyfill");
-// }
-
 // Scripts
-import accessibility from './lib/accessibility';
-import accordions from './lib/accordions';
-import horizontalScroll from './lib/horizontalScroll';
-import modal from './lib/modal';
-import navigation from './lib/navigation';
-import readMore from './lib/readMore';
-import slider from './lib/slider';
-import smoothScroll from './lib/smoothScroll';
-import tabs from './lib/tabs';
+// import accessibility from './lib/accessibility';
+// import accordions from './lib/accordions';
+// import horizontalScroll from './lib/horizontalScroll';
+// import modal from './lib/modal';
+// import navigation from './lib/navigation';
+// import readMore from './lib/readMore';
+// import slider from './lib/slider';
+// import smoothScroll from './lib/smoothScroll';
+// import tabs from './lib/tabs';
 
 /**
  * Styles
@@ -23,15 +17,15 @@ import tabs from './lib/tabs';
  */
 import '../styles/style.scss';
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('JMJ');
-  navigation();
-  accessibility();
-  smoothScroll();
-  accordions();
-  tabs();
-  readMore();
-  horizontalScroll('.lb-tabs .tabs');
-  modal();
-  slider();
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('JMJ');
+//   navigation();
+//   accessibility();
+//   smoothScroll();
+//   accordions();
+//   tabs();
+//   readMore();
+//   horizontalScroll('.lb-tabs .tabs');
+//   modal();
+//   slider();
+// });
