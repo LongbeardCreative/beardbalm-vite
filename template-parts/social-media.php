66176d45f60e654d1447b5f036ee602e296f1bca
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-06-22 14:03:19 -0500

    feat: social styles

--- template-parts/social-media.php
+++ template-parts/social-media.php
@@ -4,13 +4,15 @@ $class = $args && isset($args['class']) ? ' ' . $args['class'] : '';
 if (have_rows('social_media_platform', 'option')) {
 ?>
 
-  <ul class="social-media<?php echo $class; ?>">
-    <?php while (have_rows('social_media_platform', 'option')) {
-      the_row();
-      $name = get_sub_field('name');
-    ?>
-      <li><a href="<?php the_sub_field('link'); ?>" target="_blank" rel="noopener noreferrer" aria-label="Duc in Altum <?php echo $name; ?>"><?php the_svg($name); ?></a></li>
-    <?php } ?>
-  </ul>
+  <div class="social-list <?php echo $class; ?>">
+    <ul>
+      <?php while (have_rows('social_media_platform', 'option')) {
+        the_row();
+        $name = get_sub_field('name');
+      ?>
+        <li><a href="<?php the_sub_field('link'); ?>" target="_blank" rel="noopener noreferrer" aria-label="Biblicum <?php echo $name; ?>"><?php the_svg($name); ?></a></li>
+      <?php } ?>
+    </ul>
+  </div>
 
 <?php }
