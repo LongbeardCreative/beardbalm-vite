ad738f0d6b80bcc73eba200f16622dee301f5831
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2022-08-22 13:53:03 -0500

    feat: add base ts file for reference

---
+++ src/scripts/main-base.ts
@@ -0,0 +1,25 @@
+/**
+ * Scripts
+ */
+
+// Other imports
+
+/**
+ * Styles
+ *
+ * It is necessary to import styles to "link" a PHP page with Vite.js client
+ * Without this, liveReload will not work
+ */
+
+import '../styles/style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JMJ');
+});
+
+// If you need jQuery:
+
+// declare let jQuery: Function;
+// if (typeof jQuery !== 'undefined') {
+//   // console.log('jQuery is loaded');
+// }
